8c171dbf62f493f036919004d4800f6ba993b061
Anbarasi U <anmmagics@gmail.com>
2017-05-15 22:41:30 +0530

    Router served with some hot errors

---
+++ src/pages/NavigationPage.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  TextInput,
+  Button,
+  View
+} from 'react-native';
+
+export default class NavigationPage extends Component {
+
+  constructor() {
+      this.items = [
+        { name: 'Expenses', page: 'Expenses' },
+        { name: 'Borewell', page: 'Borewell' },
+        { name: 'Reports', page: 'Reports' },
+        { name: 'EmployeeDetails', page: 'EmployeeDetails' }
+      ];
+  }
+
+  onTileSelected() {
+    // Navigate to selected activity
+
+  }
+
+  render() {
+    return (
+      <View style = {styles.container}>
+        this.items.map((i) => {
+          return(
+            <Tile imgSrc = '' name = {i.name} link = {i.page} />
+          );
+        });
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    // TODO: Grid styling
+  }
+});
+
+AppRegistry.registerComponent('NavigationPage', () => NavigationPage);
