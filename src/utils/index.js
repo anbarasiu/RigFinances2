1e904e5393e6d3fecae5382e5e5cf6ddabdcbd7d
Anbarasi U <anmmagics@gmail.com>
2017-07-29 18:11:12 +0530

    Taming the problematic picker

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,23 +1,16 @@
+// @flow
 import React from 'react';
-import {Dimensions} from 'react-native';
-import {
-  TextInput,
-  Button,
-  Text,
-  View,
-  ScrollView,
-  Picker
-} from 'react-native';
+import { Dimensions, TextInput, Picker } from 'react-native';
 
-export function vw(percentageWidth) {
+export function vw(percentageWidth: number) {
   return Dimensions.get('window').width * (percentageWidth / 100);
 }
 
-export function vh(percentageHeight) {
+export function vh(percentageHeight: number) {
   return Dimensions.get('window').height * (percentageHeight / 100);
 }
 
-export function setResponsiveGrid(columns) {
+export function setResponsiveGrid(columns: number) {
   const COLUMNS = columns;
   const MARGIN = vw(1);
   const SPACING = (COLUMNS + 1) / COLUMNS * MARGIN;
@@ -29,15 +22,21 @@ export function setResponsiveGrid(columns) {
   };
 }
 
-export function renderComponent(type, style) {
+export function renderComponent(type: string, style: Object, content = []) {
   switch (type) {
     case 'Picker':
-    return <Picker style={style} />;
+      return (
+        <Picker style={style}>
+          {content
+            .filter(c => typeof c === 'string') //TODO: Modify after normalizing data
+            .map((c, index) => <Picker.Item key={index} label={c} value={c} />)}
+        </Picker>
+      );
     case 'Number':
-    return <TextInput style={style} keyboardType="numeric" />;
+      return <TextInput style={style} keyboardType="numeric" />;
     case 'TimePicker':
-    return <TextInput style={style} keyboardType="numeric" />;
+      return <TextInput style={style} keyboardType="numeric" />;
     default:
-    return <TextInput style={style} />;
+      return <TextInput style={style} />;
   }
 }
