d3c973217e8b531fae3b7ffb0caf05345c52ddf1
Anbarasi U <anmmagics@gmail.com>
2017-08-02 20:11:58 +0530

    Refactorringg...

--- src/constants.js
+++ src/constants.js
@@ -3,6 +3,5 @@ export default [
   { name: 'Bore Details', page: 'BoreDetails', bgColor: '' },
   { name: 'Reports', page: 'Reports', bgColor: '' },
   { name: 'Employee Details', page: 'EmployeeDetails', bgColor: '' },
-  { name: 'Master', page: 'Master', bgColor: '' },
   { name: 'Employee Master', page: 'EmployeeMaster', bgColor: '' }
 ];
--- src/pages/MasterPage.js
+++ src/pages/MasterPage.js
@@ -1,55 +1,44 @@
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  TextInput,
-  Button,
-  View
-} from 'react-native';
-import FormHeader from '../components/FormHeader';
-import content from '../content/master';
-import {renderComponent, setResponsiveGrid} from '../utils';
-import commonStyles from '../utils/styles';
-import {MasterPageScreen} from '../routes';
+import { AppRegistry } from 'react-native';
+import { Container, Tab, Tabs } from 'native-base';
+import ReportsPage from './ReportsPage';
+import ExpensesPage from './ExpensesPage';
+import BoreDetailsPage from './BoreDetailsPage';
+import EmployeeDetailsPage from './EmployeeDetailsPage';
+import EmployeeMasterPage from './EmployeeMasterPage';
 
 export default class MasterPage extends Component {
   static navigationOptions = {
-    title: 'Master',
+    title: 'Master'
   };
 
   constructor() {
     super();
-    this.columns = setResponsiveGrid(2);
   }
 
   render() {
     return (
-      <View style = {styles.container}>
-        <FormHeader />
-        {
-          content.map((c, index) => {
-            return (
-              <View key={index} style={styles.row}>
-                <Text style={this.columns}>{c.field}</Text>
-                {renderComponent(c.type, this.columns)}
-              </View>
-            );
-          })
-        }
-        <Button type = "button" title = "Submit" onPress = {this.onSubmitPressed} style = {styles.submit} />
-      </View>
+      <Container>
+        <Tabs initialPage={1}>
+          <Tab heading="Expenses">
+            <ExpensesPage />
+          </Tab>
+          <Tab heading="Bore Details">
+            <BoreDetailsPage />
+          </Tab>
+          <Tab heading="Employee Master">
+            <EmployeeMasterPage />
+          </Tab>
+          <Tab heading="Employee Details">
+            <EmployeeDetailsPage />
+          </Tab>
+          <Tab heading="Reports">
+            <ReportsPage />
+          </Tab>
+        </Tabs>
+      </Container>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-    flex: 1
-  },
-  submit: commonStyles.submit
-});
-
-AppRegistry.registerComponent('MasterPage', () => MasterPageScreen);
+AppRegistry.registerComponent('MasterPage', () => MasterPage);
