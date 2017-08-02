7ec0ad89f403a365222fdac46c059b9f1c2ddd5e
Anbarasi U <anmmagics@gmail.com>
2017-08-02 20:11:58 +0530

    Refactorringg...

--- src/routes.js
+++ src/routes.js
@@ -1,31 +1,12 @@
-import { StackNavigator, TabNavigator } from 'react-navigation';
+import { StackNavigator } from 'react-navigation';
 import LoginPage from './pages/LoginPage';
 import NavigationPage from './pages/NavigationPage';
-import BoreDetailsPage from './pages/BoreDetailsPage';
-import EmployeeDetailsPage from './pages/EmployeeDetailsPage';
-import EmployeeMasterPage from './pages/EmployeeMasterPage';
 import MasterPage from './pages/MasterPage';
-import ReportsPage from './pages/ReportsPage';
-import ExpensesPage from './pages/ExpensesPage';
-
-const TabView = TabNavigator({
-  Expenses: { screen: ExpensesPage },
-  BoreDetails: { screen: BoreDetailsPage },
-  EmployeeDetails: { screen: EmployeeDetailsPage },
-  EmployeeMaster: { screen: EmployeeMasterPage },
-  Reports: { screen: ReportsPage }
-});
 
 const App = StackNavigator({
   Login: { screen: LoginPage },
   Navigation: { screen: NavigationPage },
-  Content: { screen: TabView }
-  // Expenses: { screen: ExpensesPage },
-  // BoreDetails: { screen: BoreDetailsPage },
-  // EmployeeDetails: { screen: EmployeeDetailsPage },
-  // EmployeeMaster: { screen: EmployeeMasterPage },
-  // Master: { screen: MasterPageScreen },
-  // Reports: { screen: ReportsPage }
+  Content: { screen: MasterPage }
 });
 
-export {App, TabView};
+export { App };
